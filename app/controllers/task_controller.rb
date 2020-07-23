bcfdb110b8b42f627d8ad58229c7c135927eb16f
重田 将人 <masato.0928.j@gmail.com>
2020-07-24 08:07:12 +0900

    TODOの表示と作成ができるようにした

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,4 +2,12 @@ class TaskController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def create
+    @task = Task.new(task: params[:task])
+    if @task.valid?
+      @task.save
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-
   root 'task#index'
+  post 'tasks', to: 'task#create'
 end
